c0e73059f585626bf6f7d1b6225cf91fa2229dff
Yuchen Wang <yuchen.wang@honecap.com>
2018-08-08 14:51:11 -0700

    changed to py3 syntax

--- _printer.py
+++ _printer.py
@@ -1,35 +1,35 @@
 def print_dict(d, indent):
     if not isinstance(d, dict):
-        print d
-    print '{'
+        print(d)
+    print('{')
     for k, v in d.items():
-        for i in xrange(indent+1):
-            print ' ',
-        print k + ':',
+        for i in range((indent+1) * 2):
+            print(' ',)
+        print (k + ':',)
         if isinstance(v, dict):
             print_dict(v, indent+1)
         elif isinstance(v, list):
             print_list(v, indent+1)
         else:
-            print repr(v) + ','
-    for i in xrange(indent):
-        print ' ',
-    print '}'
+            print(repr(v) + ',')
+    for i in range(indent * 2):
+        print(' ',)
+    print('}')
 
 
 def print_list(l, indent):
     if not isinstance(l, list):
-        print l
-    print '['
+        print(l)
+    print('[')
     for v in l:
-        for i in xrange(indent+1):
-            print ' ',
+        for i in range((indent+1) * 2):
+            print(' ',)
         if isinstance(v, dict):
             print_dict(v, indent+1)
         elif isinstance(v, list):
             print_list(v, indent+1)
         else:
-            print  repr(v) + ','
-    for i in xrange(indent):
-        print ' ',
-    print ']'
+            print(repr(v) + ',')
+    for i in range(indent * 2):
+        print (' ',)
+    print(']')
